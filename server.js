b899ee00619560292fbf880ccbc920e7611b42f8
vikramartist <vikrampaiv2604@gmail.com>
2023-12-07 23:47:17 +0530

    update server.js file

--- server.js
+++ server.js
@@ -11,7 +11,11 @@ app.use(express.json());
 
 app.use("/api/contacts", require("./routes/ContactRoute.js"));
 app.use("/api/users", require("./routes/userRoute"));
+app.use("/", (req, res) => {
+  return res.json({ message: "Welcome to the Contacts App Api" });
+});
 app.use(errorHandler);
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
